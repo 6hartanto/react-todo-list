3e46fdbb66cdfa7a60fe22ee1a7d4ef247a86465
6hartanto <6hartanto@gmail.com>
2021-12-05 13:19:00 +0700

    handle display todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
 import { addTodos, removeTodos, updateTodos, completeTodos } from '../redux/reducer';
 
@@ -20,20 +20,7 @@ const mapDispatchToProps = (dispatch) => {
 const Todos = (props) => {
   const [todo, setTodo] = useState("");
 
-  const inputRef = useRef(true);
-
-  const changeFocus = () => {
-    inputRef.current.disabled = false;
-    inputRef.current.focus();
-  };
   
-  const update = (id,value,e) => {
-    if(e.which === 13) {
-      //13 is key for enter key
-      props.updateTodo({id, item:value});
-      inputRef.current.disabled = true;
-    }
-  }
 
   const handleChange = (e) => {
     setTodo(e.target.value)
@@ -50,20 +37,13 @@ const Todos = (props) => {
       })}>Add</button>
       <br />
 
-      <ul>
+      {/* <ul>
         {props.todos.map((item) => {
-          return <li key={item.id}>
-          <textarea
-            ref={inputRef}
-            disabled={inputRef}
-            defaultValue={item.item}
-            onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
-          />
-          <button onClick={() => changeFocus()}>Edit</button>
-          <button onClick={() => props.completeTodo(item.id)}>Complete</button>
-          <button onClick={() => props.removeTodo(item.id)}>Delete</button></li>
+          return (
+          
+          );
         })}
-      </ul>
+      </ul> */}
     </div>
   )
 }
